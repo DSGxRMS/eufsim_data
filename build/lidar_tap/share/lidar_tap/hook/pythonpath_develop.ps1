045290dd23c16ae15ae72220f9453cc665b1570a
ArmaanM77 <armaanmahajanbg@gmail.com>
2025-09-23 22:26:32 +0530

    Run LiDAR pipeline

---
+++ build/lidar_tap/share/lidar_tap/hook/pythonpath_develop.ps1
@@ -0,0 +1,3 @@
+# generated from colcon_powershell/shell/template/hook_prepend_value.ps1.em
+
+colcon_prepend_unique_value PYTHONPATH "$env:COLCON_CURRENT_PREFIX\/home/armaanm/eufs_dev/eufs_data/build/lidar_tap"
---
+++ src/lidar_tap/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+package_name = 'lidar_tap'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='armaanm',
+    maintainer_email='armaanmahajanbg@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'velodyne_tap = lidar_tap.pointcloud_velodyne:main',
+            'pcviewer = lidar_tap.lidarview:main'
+        ],
+    },
+)
