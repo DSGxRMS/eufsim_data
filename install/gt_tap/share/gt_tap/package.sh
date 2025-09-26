ac76cfb6cc7f6197e0d395f346aa61c266eae9d1
ArmaanM77 <armaanmahajanbg@gmail.com>
2025-09-26 09:28:42 +0530

    Update Ground Truth API

--- install/gt_tap/share/gt_tap/package.sh
+++ install/gt_tap/share/gt_tap/package.sh
@@ -80,7 +80,6 @@ _colcon_package_sh_source_script() {
 # source sh hooks
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/gt_tap/hook/pythonpath.sh"
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/gt_tap/hook/ament_prefix_path.sh"
-_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/../../build/gt_tap/share/gt_tap/hook/pythonpath_develop.sh"
 
 unset _colcon_package_sh_source_script
 unset COLCON_CURRENT_PREFIX
